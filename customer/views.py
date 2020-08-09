cad6d6f229388e5d1a50e5efea7a30539b0af954
RitikaSingh02 <ritika2002singh@gmail.com>
2020-08-09 15:13:09 +0530

    django sessions

--- customer/views.py
+++ customer/views.py
@@ -11,7 +11,10 @@ def login(request):
         password=data['password']
         user=users.objects.filter(email__contains=email,password__contains=password).filter(email=email,password=password).values()
         if(user):
+            request.session["email"] = data['email']
+            request.session['password'] =data['password']
             return JsonResponse(list(user),safe=False)
+
         else:
             return JsonResponse("wrong",safe=False)
     
@@ -29,15 +32,31 @@ def signup(request):
             phone=phone,
             email=email
         )
-        request.session["email"] = data['email']
+        
         return JsonResponse("yes",safe=False)
 
 def logout(request):
     try:
         del request.session['email']
+        del request.session['password']
+        #The output will look like this and don’t worry if your cookie didn’t delete
+        # because we use this method only to delete your data in the Django database and not the session ID and cookie itself.
+        #you can check it by access_session funct it would raise an error coz no session will be there
+        #to completely delete use flush()
+       
     except:
         pass
     return JsonResponse("logout successfull",safe=False)
 
+
+
+
+# def access_session(request):
+    
+#     if request.session.get('email'):
+#         return JsonResponse(request.session.get('password'),safe=False)
+  
+
+    
 
 # Create your views here.
