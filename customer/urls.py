66e0d56429824e7889c1cd42be78cff4ef210911
RitikaSingh02 <ritika2002singh@gmail.com>
2020-08-09 15:12:46 +0530

    e_commerce

--- customer/urls.py
+++ customer/urls.py
@@ -4,6 +4,10 @@ from . import views
 urlpatterns=[
    url(r'^login/$',views.login,name="login"),
    url(r'^signup/$',views.signup,name="login"), 
-    url(r'^logout/$',views.logout,name="login"), 
+   url(r'^logout/$',views.logout,name="login"), 
+   url(r'^set/$',views.cookie_session,name="login"),
+   url(r'^del/$',views.cookie_delete,name="login"),
+   url(r'^access_session/$',views.access_session,name="login"),
+    
 
 ]
