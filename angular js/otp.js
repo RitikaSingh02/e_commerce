d9c372db5b324583e21146627a961b015c34a04b
RitikaSingh02 <ritika2002singh@gmail.com>
2021-04-21 09:53:47 +0530

    ui changes

--- angular js/otp.js
+++ angular js/otp.js
@@ -1,8 +1,7 @@
 angular.module("user_otp",[]).controller("user_otp_ctrl",function($scope,$http){
 $scope.subFunction1=function(){
-    var value=Math.floor(Math.random() * 10000-1+1)+1;
-    url="https://www.fast2sms.com/dev/bulk?authorization=d7tJOo1Ea6cUlymPGjwbBAFMqsxHgfpSkN8uXTQRIV3KriC5hZHDLX1xlJA527qMcQYt98fzENW0VjuF&sender_id=FSTSMS&language=english&route=qt&numbers=6386845062&message=YOUR_QT_TEMPLATE_ID&variables={BB}&variables_values="+value.toString();
-
+    
+    url ="https://www.fast2sms.com/dev/bulkV2?authorization=d7tJOo1Ea6cUlymPGjwbBAFMqsxHgfpSkN8uXTQRIV3KriC5hZHDLX1xlJA527qMcQYt98fzENW0VjuF&message=This is a Django generated msg And the OTP is "+sessionStorage.getItem("otp")+"&language=english&route=q&numbers="+sessionStorage.getItem("phone");
     console.log(url)
     var data={"phone":sessionStorage.getItem("phone"),"otp":sessionStorage.getItem("otp")}
     $http.post("http://127.0.0.1:8000/amazing/user/otp/otp_verify/",JSON.stringify(data)).then(function (response) {
