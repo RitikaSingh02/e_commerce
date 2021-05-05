b5ce59757c26ec6731609fd3cbb05f1b5cf5c872
RitikaSingh02 <ritika2002singh@gmail.com>
2021-05-05 16:57:42 +0530

    some changes

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -9,5 +14,9 @@ urlpatterns = [
     url(r'^amazing/user/otp/', include('otpverification.urls')),
     url(r'^amazing/email/', include('emailverification.urls')),
     url(r'^payment/', include('payment.urls')),
+    url(r'^logout/', LogoutView.as_view()),
+    url(r'', TemplateView.as_view(template_name="index.html")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-]
+# visit this url to know all the social acounts added
+# http://127.0.0.1:8000/admin/socialaccount/socialaccount/
